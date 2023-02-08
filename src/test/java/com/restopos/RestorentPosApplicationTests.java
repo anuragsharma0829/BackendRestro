3efe36fe0a7ebffd47c6f4c24a525f85806d3f2f
Anurag Sharma <anurag.sharma-int@zehntech.com>
2023-02-08 14:39:13 +0530

    Updatted

---
+++ src/test/java/com/restopos/RestorentPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restopos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorentPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
